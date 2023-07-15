e8111783ed7a6d6f0634e03bc927ce10d9c5560d
Teferi Seyoum <tefieseyoum14@gmail.com>
2023-07-15 08:59:07 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -A a program that prints exactly
+ * Return: Always 1 (if the program terminates succussfuly).
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}

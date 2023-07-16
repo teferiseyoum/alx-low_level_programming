80130e31f89b35b85817433dce22791bb92666f4
Teferi Seyoum <tefieseyoum14@gmail.com>
2023-07-16 08:02:19 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -main function
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 9 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

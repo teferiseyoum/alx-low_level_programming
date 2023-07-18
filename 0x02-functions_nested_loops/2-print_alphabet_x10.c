d550a9ff418387c9f030635d7e9ab54ff49a9b38
Teferi Seyoum <tefieseyoum14@gmail.com>
2023-07-18 02:58:08 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -Make alphabet x10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char t;
+	int i = 0;
+
+	while (i <= 9)
+		for (t = 'a'; t <= 'z' ; t++)
+		{
+			_putchar(t);
+			_putchar('\n');
+			i++;
+		}
+}

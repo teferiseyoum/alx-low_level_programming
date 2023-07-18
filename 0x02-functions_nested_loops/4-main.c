363f0f4e1a9211ed718b53686df5baa36b1d4552
Teferi Seyoum <tefieseyoum14@gmail.com>
2023-07-18 04:12:52 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -9,11 +9,12 @@
 int main(void)
 {
 	int r;
+
 	r = _isalpha('H');
 	_putchar(r + '0');
 	r = _isalpha('o');
 	_putchar(r + '0');
-	r= _isalpha(108);
+	r = _isalpha(108);
 	_putchar(r + '0');
 	r = _isalpha(';');
 	_putchar(r + '0');

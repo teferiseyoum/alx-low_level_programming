d59c81da2bd1c2dca89a1e79beecfc0b85b267a9
Teferi Seyoum <tefieseyoum14@gmail.com>
2023-07-18 02:42:47 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char t;
+
+	for (t = 'a' ; t <= 'z' ; t++)
+	{
+		_putchar(t);
+	}
+	_putchar('\n');
+}

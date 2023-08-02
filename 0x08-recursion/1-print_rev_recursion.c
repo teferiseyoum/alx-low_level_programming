1b3a46847a621e688daa8f3413b3679df3291279
Teferi Seyoum <tefieseyoum14@gmail.com>
2023-08-02 08:57:06 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+ * _print_rev_recursion - print string  in reversed
+ *
+ * @s: the stringb to be reversed
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}

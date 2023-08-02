6d6e551f21488427774139713ffd1e447f9ad196
Teferi Seyoum <tefieseyoum14@gmail.com>
2023-08-02 12:38:15 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,30 +13,31 @@ int _strlen_recursion(char *s)
 		return (1 + _strlen_recursion(s + 1));
 }
 /**
- * compare_string compare each caracterof the string
+ * comparator - compare each caracterof the string.
  * @s: string
- * @left: smallest iterator
- * @right: largest iterator
- * return: int
+ * @n1: smallest iterator.
+ * @n2: biggest iterator.
+ * return: .
  */
-int compare_string(char *s, int left, int right)
+
+int comparator(char *s, int n1, int n2)
 {
-	if (*(s + left) == *(s + right))
+	if (*(s + n1) == *(s + n2))
 	{
-		if (left == right || left == right + 1)
+		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
-		return (0 + compare_string(s, left + 1, right - 1));
+		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
 }
 /**
  * is_palindrome - detect if string
  * @s: the string
- * Return: 1 if it is a pali..., 0 if not
+ * Return: 1 if it is a pali...., 0 if not
  */
 int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	return (compare_string(s, 0, _strlen_recursion(s) - 1));
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
